0f73bc0452d8566d13af79a972412c9edc9bd8c9
raul-dunca <109088288+raul-dunca@users.noreply.github.com>
2023-06-28 18:39:22 +0300

    Add files via upload

---
+++ Controllers/MainController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Asp_Lab.DataAbstractionLayer;
+using Asp_Lab.Models;
+
+namespace Asp_Lab.Controllers
+{
+    public class MainController : Controller
+    {
+        // GET: Main
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+        public string Test()
+        {
+            return "It's working";
+        }
+
+        public ActionResult GetDocuments()
+        {
+            DAL dal = new DAL();
+            List<Document> doc_list = dal.GetAllDocuments();
+            ViewData["documentList"] = doc_list;
+            return View("GetDocuments");
+        }
+
+
+        public ActionResult AddDocument()
+        {
+            
+            return View("AddDocument");
+        }
+
+        public ActionResult Login()
+        {
+
+            return View("Login");
+        }
+
+        public ActionResult ErrorLogin()
+        {
+
+            return View("ErrorLogin");
+        }
+
+       
+
+        public String Check(string username,string password)
+        {
+            DAL dal = new DAL();
+            
+            bool ok= dal.Login(username, password);
+
+            if (ok)
+            {
+                return "true";
+            }
+            else
+            {
+                return "false";
+            }
+        }
+
+        public ActionResult DeleteDocument(int id)
+        {
+
+            DAL dal = new DAL();
+            dal.DeleteDocument(id);
+            return RedirectToAction("GetDocuments");
+            //List<Document> doc_list = dal.GetAllDocuments();
+            //ViewData["documentList"] = doc_list;
+            //return View("GetDocuments");
+        }
+
+        public ActionResult SaveDocument()
+        {
+            Document document = new Document();
+            document.Author = Request.Params["author"];
+            document.Title = Request.Params["title"];
+            document.NrOfPages = int.Parse(Request.Params["nr_of_pages"]);
+            document.Type = Request.Params["type"];
+            document.Format = Request.Params["format"];
+            
+            DAL dal = new DAL();
+            dal.AddDocument(document);
+
+            //return "don";
+            return RedirectToAction("GetDocuments");
+            //return View("GetDocuments");
+        }
+
+
+
+        public ActionResult UpdateDocument(int id, string author, string title, int pages, string type, string format)
+        {
+           
+            
+            ViewData["id"] = int.Parse(Request.Params["id"]); ;
+            ViewData["auth"] = Request.Params["author"]; ;
+            ViewData["title"] = Request.Params["title"];
+            ViewData["pages"] = int.Parse(Request.Params["pages"]);
+            ViewData["type"] = Request.Params["type"];
+            ViewData["format"] = Request.Params["format"];
+            return View("UpdateDocument");
+        }
+
+
+        public ActionResult SaveUpdateDocument(int id, string author,string title,int nr_of_pages,string type,string format)
+        {
+            
+            
+            DAL dal = new DAL();
+            dal.UpdateDocument(id,author,title,nr_of_pages,type,format);
+            List<Document> doc_list = dal.GetAllDocuments();
+            ViewData["documentList"] = doc_list;
+            return View("GetDocuments");
+        }
+
+        public String FilterDocuments(string type, string format)
+        {
+
+
+            DAL dal = new DAL();
+            List<Document> doc_list = dal.GetAllDocuments(type,format);
+            ViewData["documentList"] = doc_list;
+
+            string result = "<table border='1'>" +
+                "<tr>" +
+                "<th>Id</th>" +
+                "<th>Author</th>" +
+                "<th>Title</th>" +
+                "<th>NrOfPages</th>" +
+                "<th>Type</th>" +
+                "<th>Format</th>" +
+                "<th>Operation</th>" +
+                "</tr>";
+
+
+            foreach (Document d in doc_list)
+            {
+                result += "<tr><td>" + d.Id + "</td><td>" + d.Author + "</td><td>" + d.Title + "</td><td>" + d.NrOfPages + "</td><td>"+ d.Type + "</td><td>"+ d.Format + "</td><td>"+ "<button class='edit-btn' data-id='" + d.Id + "' data-author='" + d.Author + "' data-title='" + d.Title + "' data-pages='" + d.NrOfPages + "' data-type='" + d.Type + "' data-format='" + d.Format + "'>Edit</button>" +
+"<button class='delete-btn' data-id='" + d.Id + "'>Delete</button>" + "</td>"+"</tr>";
+            }
+
+            result += "</table>";
+            return result;
+        }
+
+        
+
+    }
+}
---
+++ DataAbstractionLayer/DAL.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Asp_Lab.Models;
+using MySql.Data.MySqlClient;
+
+namespace Asp_Lab.DataAbstractionLayer
+{
+    public class DAL
+    {
+        public List<Document> GetAllDocuments(string type=null, string format=null)
+        {
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=localhost;uid=root;pwd=;database=db_documents;";
+            List<Document> document_list = new List<Document>();
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+
+                if (!string.IsNullOrEmpty(type) && !string.IsNullOrEmpty(format))
+                {
+                    cmd.CommandText = "select * from documents WHERE type LIKE '%' '"+type+ "' '%' AND format LIKE '%' '" + format + "' '%'";
+                }
+                else
+                {
+                    if (string.IsNullOrEmpty(type) && !string.IsNullOrEmpty(format))
+                    {
+                        cmd.CommandText = "select * from documents WHERE format LIKE '%' '" + format + "' '%'";
+                    }
+                    else
+                    {
+                        if (!string.IsNullOrEmpty(type) && string.IsNullOrEmpty(format))
+                        {
+                            cmd.CommandText = "select * from documents WHERE type LIKE '%' '" + type + "' '%'";
+                        }
+                        else
+                        {
+                            cmd.CommandText = "select * from documents";
+                        }
+                    }
+                }
+
+                
+                MySqlDataReader myreader = cmd.ExecuteReader();
+
+                while (myreader.Read())
+                {
+                    Document document = new Document();
+                    document.Id = myreader.GetInt32("id");
+                    document.Author = myreader.GetString("author");
+                    document.Title = myreader.GetString("title");
+                    document.NrOfPages = myreader.GetInt32("nr_of_pages");
+                    document.Type = myreader.GetString("type");
+                    document.Format = myreader.GetString("format");
+                    document_list.Add(document);
+                }
+                myreader.Close();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.Write(ex.Message);
+            }
+            return document_list;
+        }
+
+
+        public void AddDocument(Document document)
+        {
+            
+
+
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=localhost;uid=root;pwd=;database=db_documents;";
+            
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "INSERT INTO documents (author, title, nr_of_pages, type, format) VALUES (@Author, @Title, @NrOfPages, @Type, @Format)";
+
+                // Add parameters to the command
+                cmd.Parameters.AddWithValue("@Author", document.Author);
+                cmd.Parameters.AddWithValue("@Title", document.Title);
+                cmd.Parameters.AddWithValue("@NrOfPages", document.NrOfPages);
+                cmd.Parameters.AddWithValue("@Type", document.Type);
+                cmd.Parameters.AddWithValue("@Format", document.Format);
+
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.Write(ex.Message);
+            }
+            
+        }
+
+
+
+        public void UpdateDocument(int id,string new_author, string new_title, int new_nr_of_pages, string new_type, string new_format)
+        {
+
+
+
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=localhost;uid=root;pwd=;database=db_documents;";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "UPDATE documents SET author=@Author, title=@Title, nr_of_pages=@NrOfPages, type=@Type, format=@Format WHERE id=@Id";
+
+                // Add parameters to the command
+                cmd.Parameters.AddWithValue("@Author", new_author);
+                cmd.Parameters.AddWithValue("@Title", new_title);
+                cmd.Parameters.AddWithValue("@NrOfPages", new_nr_of_pages);
+                cmd.Parameters.AddWithValue("@Type", new_type);
+                cmd.Parameters.AddWithValue("@Format", new_format);
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+        }
+
+        public void DeleteDocument(int id)
+        {
+
+
+
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=localhost;uid=root;pwd=;database=db_documents;";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "DELETE FROM documents WHERE id=@Id";
+
+                cmd.Parameters.AddWithValue("@Id", id);
+
+                cmd.ExecuteNonQuery();
+
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+        }
+
+
+        public bool Login(string username , string password)
+        {
+
+
+
+            MySql.Data.MySqlClient.MySqlConnection conn;
+            string myConnectionString;
+
+            myConnectionString = "server=localhost;uid=root;pwd=;database=db_users;";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+
+                MySqlCommand cmd = new MySqlCommand();
+                cmd.Connection = conn;
+                cmd.CommandText = "SELECT * FROM users WHERE username=@Username AND password=@Pass";
+
+                cmd.Parameters.AddWithValue("@Username", username);
+                cmd.Parameters.AddWithValue("@Pass", password);
+
+                MySqlDataReader myreader = cmd.ExecuteReader();
+                if (!myreader.HasRows)
+                {
+                    return false;
+                }
+                
+                
+                return true;
+                
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.Write(ex.Message);
+                return false;
+            }
+
+        }
+    }
+}
