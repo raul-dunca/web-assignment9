4a37e7ca96b7da92a323a91c1503e039259d3818
raul-dunca <109088288+raul-dunca@users.noreply.github.com>
2023-06-28 18:36:28 +0300

    Add files via upload

---
+++ Models/Document.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Asp_Lab.Models
+{
+    public class Document
+    {
+        public int Id { get; set; }
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public int NrOfPages { get; set; }
+        public string Type { get; set; }
+        public string Format { get; set; }
+    }
+}
